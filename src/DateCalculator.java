b99b3ce1260ff89ad8e361e04ba5c24cc59e3b2e
jburge <jburge@coloradocollege.edu>
2021-09-08 17:21:16 -0600

    added a constant

--- src/DateCalculator.java
+++ src/DateCalculator.java
@@ -3,6 +3,7 @@ public class DateCalculator {
 	
 	public static final int ORIGIN_YEAR = 1980;
 	public static final int DAYS_IN_A_YEAR = 365;
+	public static final int UNUSED = 0;
 	
 	private int daysRemaining;
 	private int year;
